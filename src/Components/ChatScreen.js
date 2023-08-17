3678d21a4964660f298a7512d117434a500e69f5
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2023-08-17 11:44:07 +0530

    fiexd

--- src/Components/ChatScreen.js
+++ src/Components/ChatScreen.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function ChatScreen() {
-    let person1=[]
-    let person2=[]
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    setMessage(event.target.value);
+  };
+
   return (
-    <div>
-      
+    <div className="flex justify-center items-center h-screen">
+      <div className="container mx-auto bg-black p-4 h-96 w-48">
+        <p className="text-white break-words">Message: {message}</p>
+        <div className="flex justify-center items-end">
+          <div className="mb-2">
+            <p className="text-white ">Type your message:</p>
+            <input
+              name="message"
+              className="bg-gray-800 w-full p-2 text-white"
+              type="text"
+              value={message}
+              onChange={handleSubmit}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatScreen
+export default ChatScreen;
